96b160c555777a9d0c3f3a38256bd708d30228ba
Natasha Romm <romm.natasha@gmail.com>
2022-04-07 17:26:12 +0300

    Fixes - now works!

--- gke/gke_patches.py
+++ gke/gke_patches.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import subprocess
 
 
@@ -30,6 +31,10 @@ def exec_command(command):
     output = subprocess.run(command.split(), stdout=subprocess.PIPE)
     return str(output.stdout, 'utf-8') if output is not None else ""
 
+def exec_string_command(string_command):
+    output = subprocess.run(string_command, stdout=subprocess.PIPE, shell=True)
+    return str(output.stdout, 'utf-8') if output is not None else ""
+
 def apply_yaml(yaml_content):
     yaml_filepath = '/tmp/yaml_to_deploy.yaml'
     write_to_file(yaml_content, yaml_filepath)
@@ -52,7 +57,7 @@ def add_nvidia_volumes(gfd_yaml_line):
 def remove_priority_class(gfd_yaml_line):
     if 'priorityClassName' in gfd_yaml_line:
         debug_print('Removing priorityClassName from gpu-feature-discovery')
-        return ''
+        return "      priorityClassName: null"
     return gfd_yaml_line
 
 def get_gfd_yaml():
@@ -79,28 +84,37 @@ def edit_gfd():
 def get_dcgm_exporter_namespace_from_args():
     return sys.argv[1] if len(sys.argv) > 1 else DCGM_EXPORTER_NAMESPACE
 
-def get_dcgm_exporter_yaml():
+def get_dcgm_exporter_yaml(dcgm_exporter_namespace):
     debug_print('Getting dcgm-exporter yaml')
-    get_dcgm_exporter_yaml_command = 'kubectl get ds dcgm-exporter -n {} -oyaml'.format(get_dcgm_exporter_namespace_from_args())
+    get_dcgm_exporter_yaml_command = 'kubectl get ds dcgm-exporter -n {} -oyaml'.format(dcgm_exporter_namespace)
     return exec_command(get_dcgm_exporter_yaml_command)
 
 def edit_dcgm_exporter_yaml(dcgm_exporter_yaml):
     edited_dcgm_exporter = ''
     for line in dcgm_exporter_yaml.splitlines():
-        edited_line = add_nvidia_volumes(edited_line)
+        edited_line = add_nvidia_volumes(line)
         edited_dcgm_exporter += edited_line + '\n'
 
     return edited_dcgm_exporter
 
-def edit_dcgm_exporter():
-    dcgm_exporter_yaml = get_dcgm_exporter_yaml()
+def edit_dcgm_exporter(dcgm_exporter_namespace):
+    dcgm_exporter_yaml = get_dcgm_exporter_yaml(dcgm_exporter_namespace)
     dcgm_exporter_yaml = edit_dcgm_exporter_yaml(dcgm_exporter_yaml)
     debug_print('Applying edited dcgm-exporter')
     apply_yaml(dcgm_exporter_yaml)
 
+################ dcgm-exporter ################
+def patch_runaiconfig(dcgm_exporter_namespace):
+    debug_print('Patching runaiconfig with dcgm-exporter namespace')
+    patch_command = 'kubectl patch RunaiConfig runai -n runai -p \'{"spec": {"global": {"gpuOperator": {"namespace": "%s"}}}}\' --type="merge"' % (dcgm_exporter_namespace, )
+    exec_string_command(patch_command)
+
 def main():
+    dcgm_exporter_namespace = get_dcgm_exporter_namespace_from_args()
+
     edit_gfd()
-    edit_dcgm_exporter()
+    edit_dcgm_exporter(dcgm_exporter_namespace)
+    patch_runaiconfig(dcgm_exporter_namespace)
 
 if __name__ == "__main__":
     main()
